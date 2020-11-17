640b81f3d650e84426b26c30973274a081002f04
manuboafo <boafomanu5@gmail.com>
2020-11-17 13:42:44 +0000

    Create index.js
    grading system

---
+++ index.js
@@ -0,0 +1,25 @@
+function number(num) {
+	switch(num){
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+			return "low";
+			break;
+		case 5:
+		case 6:
+		case 7:
+			return "mid";
+			break;
+		case 8:
+		case 9:
+		case 10:
+			return "high";
+			break;
+		default :
+		return "Out of range";
+	}
+	
+}
+
+console.log(number(4));
